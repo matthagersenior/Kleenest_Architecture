b3306e02b330c310c1be3d76ada994f1d336675e
matthagersenior <matthagersr@gmail.com>
2026-08-22 08:50:00 -0500

    fix: align Architecture router with GitHub Pages base path

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Kleenest_Architecture">
       <AppProvider>
         <CanonicalAppRuntime />
       </AppProvider>
