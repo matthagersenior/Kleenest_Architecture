acd67565499571c57410a90d0d47ccafd6a81e72
matthagersenior <matthagersr@gmail.com>
2026-08-22 04:27:17 -0500

    feat: mount canonical app context at runtime boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import CanonicalAppRuntime from './CanonicalAppRuntime.jsx';
+import { AppProvider } from './AppContext.jsx';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CanonicalAppRuntime />
+      <AppProvider>
+        <CanonicalAppRuntime />
+      </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
