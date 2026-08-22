e6e2ba40a7f5e01e5e08a4357226850837428243
matthagersenior <matthagersr@gmail.com>
2026-08-22 04:59:04 -0500

    feat: add canonical consumer favorites adapter

---
+++ src/domains/consumer/favorites.js
@@ -0,0 +1,22 @@
+export function createFavoriteService(client) {
+  if (!client) throw new Error('Supabase client is required.');
+  return Object.freeze({
+    list: async () => {
+      const { data, error } = await client.rpc('my_favorite_locations');
+      if (error) throw error;
+      return data;
+    },
+    toggle: async locationId => {
+      if (!locationId) throw new Error('Location is required.');
+      const { data, error } = await client.rpc('kleenest_toggle_favorite', { p_location_id: locationId });
+      if (error) throw error;
+      return data;
+    },
+    metrics: async locationId => {
+      if (!locationId) throw new Error('Location is required.');
+      const { data, error } = await client.rpc('location_favorite_route_metrics', { p_location_id: locationId });
+      if (error) throw error;
+      return data;
+    }
+  });
+}
