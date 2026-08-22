d234ebeb0560c3d7c948fffaf7296b64c626efad
matthagersenior <matthagersr@gmail.com>
2026-08-22 07:03:27 -0500

    feat: complete enterprise partner analytics service parity

--- src/domains/partners/programs.js
+++ src/domains/partners/programs.js
@@ -1,10 +1,16 @@
 export function createPartnerProgramService(client) {
   if (!client) throw new Error('Supabase client is required.');
+  async function rpc(name, args = {}) { const { data, error } = await client.rpc(name, args); if (error) throw error; return data; }
   return Object.freeze({
-    list: async () => { const { data, error } = await client.rpc('business_list_partner_programs'); if (error) throw error; return data ?? []; },
-    memberships: async () => { const { data, error } = await client.rpc('list_my_partner_memberships'); if (error) throw error; return data ?? []; },
-    join: async (programId) => { const { data, error } = await client.rpc('join_partner_program', { p_program_id: programId }); if (error) throw error; return data; },
-    requestAgreement: async (programId, partnerBusinessId) => { const { data, error } = await client.rpc('business_request_partner_agreement', { p_partner_program_id: programId, p_partner_business_id: partnerBusinessId }); if (error) throw error; return data; },
-    acceptAgreement: async (agreementId) => { const { data, error } = await client.rpc('accept_partner_agreement', { p_agreement_id: agreementId }); if (error) throw error; return data; }
+    list: () => rpc('business_list_partner_programs').then(data => data ?? []),
+    memberships: () => rpc('list_my_partner_memberships').then(data => data ?? []),
+    join: programId => rpc('join_partner_program', { p_program_id: programId }),
+    requestAgreement: (programId, partnerBusinessId) => rpc('business_request_partner_agreement', { p_partner_program_id: programId, p_partner_business_id: partnerBusinessId }),
+    acceptAgreement: agreementId => rpc('accept_partner_agreement', { p_agreement_id: agreementId }),
+    benchmarkAnalytics: () => rpc('enterprise_partner_benchmark_analytics'),
+    campaignRoi: () => rpc('enterprise_campaign_roi_analytics'),
+    allocationRoi: () => rpc('enterprise_allocation_roi_analytics'),
+    campaignAnalytics: () => rpc('enterprise_campaign_analytics'),
+    fleetAccess: () => rpc('enterprise_fleet_access_summary')
   });
 }
