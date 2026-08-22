5ddfd9fbcd1c6de4fad6061536c000aecb10edc5
matthagersenior <matthagersr@gmail.com>
2026-08-22 07:04:25 -0500

    fix: align enterprise partner service with live Supabase RPC contracts

--- src/domains/partners/programs.js
+++ src/domains/partners/programs.js
@@ -7,10 +7,16 @@ export function createPartnerProgramService(client) {
     join: programId => rpc('join_partner_program', { p_program_id: programId }),
     requestAgreement: (programId, partnerBusinessId) => rpc('business_request_partner_agreement', { p_partner_program_id: programId, p_partner_business_id: partnerBusinessId }),
     acceptAgreement: agreementId => rpc('accept_partner_agreement', { p_agreement_id: agreementId }),
-    benchmarkAnalytics: () => rpc('enterprise_partner_benchmark_analytics'),
-    campaignRoi: () => rpc('enterprise_campaign_roi_analytics'),
-    allocationRoi: () => rpc('enterprise_allocation_roi_analytics'),
-    campaignAnalytics: () => rpc('enterprise_campaign_analytics'),
-    fleetAccess: () => rpc('enterprise_fleet_access_summary')
+    network: (networkId, start, end) => rpc('get_enterprise_partner_network', { p_network_id: networkId, ...(start ? { p_start: start } : {}), ...(end ? { p_end: end } : {}) }),
+    benchmarkAnalytics: (networkId, start, end) => rpc('get_partner_network_benchmark', { p_network_id: networkId, ...(start ? { p_start: start } : {}), ...(end ? { p_end: end } : {}) }),
+    campaignRoi: (campaignId, start, end) => rpc('get_partner_campaign_roi', { p_campaign_id: campaignId, ...(start ? { p_start: start } : {}), ...(end ? { p_end: end } : {}) }),
+    allocationRoi: (networkId, start, end) => rpc('get_partner_allocation_roi', { p_network_id: networkId, ...(start ? { p_start: start } : {}), ...(end ? { p_end: end } : {}) }),
+    preferredAnalytics: (businessId, start, end) => rpc('partner_preferred_analytics', { p_business_id: businessId, ...(start ? { p_start: start } : {}), ...(end ? { p_end: end } : {}) }),
+    createCampaign: (networkId, name, campaignType = 'engagement', goal = null) => rpc('create_enterprise_partner_campaign', { p_network_id: networkId, p_name: name, p_campaign_type: campaignType, p_goal: goal }),
+    activateCampaign: campaignId => rpc('activate_enterprise_partner_campaign', { p_campaign_id: campaignId }),
+    pauseCampaign: campaignId => rpc('pause_enterprise_partner_campaign', { p_campaign_id: campaignId }),
+    recordCampaignOutcome: (campaignId, partnerBusinessId, metrics = {}) => rpc('record_enterprise_partner_campaign_outcome', { p_campaign_id: campaignId, p_partner_business_id: partnerBusinessId, p_visits: metrics.visits ?? 0, p_check_ins: metrics.checkIns ?? 0, p_reviews: metrics.reviews ?? 0, p_preferred_uses: metrics.preferredUses ?? 0, p_access_redemptions: metrics.accessRedemptions ?? 0, p_promotion_redemptions: metrics.promotionRedemptions ?? 0, p_attributed_users: metrics.attributedUsers ?? 0, p_points_awarded: metrics.pointsAwarded ?? 0 }),
+    recordNetworkMetric: (networkId, metricDate, metrics = {}) => rpc('record_enterprise_partner_metric', { p_network_id: networkId, p_metric_date: metricDate, p_visits: metrics.visits ?? 0, p_check_ins: metrics.checkIns ?? 0, p_reviews: metrics.reviews ?? 0, p_preferred_uses: metrics.preferredUses ?? 0, p_access_redemptions: metrics.accessRedemptions ?? 0, p_promotion_redemptions: metrics.promotionRedemptions ?? 0 }),
+    fleetAccess: userId => rpc('enable_enterprise_fleet_service', { p_user_id: userId })
   });
 }
