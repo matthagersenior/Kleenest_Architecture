bfc88e8951fa894606627fb21e751da2c47d6c93
matthagersenior <matthagersr@gmail.com>
2026-08-22 21:28:23 -0500

    Make routing reliable on GitHub Pages with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import CanonicalAppRuntime from './CanonicalAppRuntime.jsx';
 import { AppProvider } from './AppContext.jsx';
 import RuntimeErrorBoundary from './runtime/RuntimeErrorBoundary.jsx';
@@ -10,12 +10,12 @@ const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Kleenest_Architecture">
+    <HashRouter>
       <RuntimeErrorBoundary>
         <AppProvider>
           <CanonicalAppRuntime />
         </AppProvider>
       </RuntimeErrorBoundary>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
