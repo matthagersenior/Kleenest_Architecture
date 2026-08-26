d2a1fed6332dffb43a7a961c20fb86c164d230f1
matthagersenior <matthagersr@gmail.com>
2026-08-26 00:20:54 -0500

    Make canonical app runtime the sole route owner

--- src/runtime/MapRouteOverride.jsx
+++ src/runtime/MapRouteOverride.jsx
@@ -1,9 +1,5 @@
-import { useLocation } from 'react-router-dom';
 import CanonicalAppRuntime from './CanonicalAppRuntime.jsx';
-import MapSurfaceV3 from './MapSurfaceV3.jsx';
 
 export default function MapRouteOverride(){
-  const { pathname } = useLocation();
-  if(pathname==='/map'||pathname==='/discover') return <MapSurfaceV3/>;
   return <CanonicalAppRuntime/>;
 }
