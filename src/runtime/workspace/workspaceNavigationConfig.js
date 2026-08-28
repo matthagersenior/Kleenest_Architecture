fd4dc6f4c835ecea31964efea9920caad1a4a7b8
matthagersenior <matthagersr@gmail.com>
2026-08-28 01:44:05 -0500

    refactor: extract workspace navigation presentation config

---
+++ src/runtime/workspace/workspaceNavigationConfig.js
@@ -0,0 +1,5 @@
+import { Bell, Shield, Sparkles, Users, Truck, Building2, UserCircle, LogIn, Lock, Database, Route, BarChart3, BriefcaseBusiness, MapPin } from 'lucide-react';
+
+export const WORKSPACE_ICONS = Object.freeze({ consumer: Sparkles, business: Building2, fleet: Truck, enterprise: Users, admin: Shield });
+export const NAV_SECTION_ICONS = Object.freeze({ Manage: BriefcaseBusiness, Engage: Sparkles, Insights: BarChart3, Account: UserCircle, Operate: Route, Governance: Shield, Platform: Database });
+export const NAV_ACTION_ICONS = Object.freeze({ account: UserCircle, notifications: Bell, signIn: LogIn, locked: Lock, database: Database, map: MapPin });
